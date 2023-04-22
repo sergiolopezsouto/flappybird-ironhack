42737e9d651b85a602c06bd447fc4cc2945773e0
sergiolopez <sergio.lopez.souto@gmail.com>
2023-04-22 16:59:30 +0200

    adding the second pipeline each interval

--- src/pipeline.js
+++ src/pipeline.js
@@ -4,7 +4,7 @@ class Pipeline {
         this.canvasSize = canvasSize
 
         this.pipelineSpecs = {
-            size: { width: 25 , height: 200 } ,
+            size: { width: 200 , height: 300 } ,
             pos: { x: this.canvasSize.w - 100 , y: this.canvasSize.h - 200} ,
             vel: 2.5 
         }
@@ -12,6 +12,7 @@ class Pipeline {
 
     draw() {
         this.ctx.fillRect(this.pipelineSpecs.pos.x, this.pipelineSpecs.pos.y, this.pipelineSpecs.size.width, this.pipelineSpecs.size.height)
+        this.ctx.fillRect(this.pipelineSpecs.pos.x, 0, this.pipelineSpecs.size.width, this.pipelineSpecs.size.height)
         this.move()
     }
 
