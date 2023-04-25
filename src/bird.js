1a4a818661df60bc564f600edeb3564198a9cc30
sergiolopez <sergio.lopez.souto@gmail.com>
2023-04-25 18:19:29 +0200

    cleaning commented code

--- src/bird.js
+++ src/bird.js
@@ -23,16 +23,16 @@ class Bird {
 
     draw() {
 
-        this.ctx.fillStyle = 'yellow'
-        this.ctx.fillRect(this.birdSpecs.pos.x , this.birdSpecs.pos.y , this.birdSpecs.size.w , this.birdSpecs.size.h)
+        // this.ctx.fillStyle = 'yellow'
+        // this.ctx.fillRect(this.birdSpecs.pos.x , this.birdSpecs.pos.y , this.birdSpecs.size.w , this.birdSpecs.size.h)
 
-        // this.ctx.drawImage(
-        //     this.imageInstance,
-        //     this.birdSpecs.pos.x,
-        //     this.birdSpecs.pos.y,
-        //     this.birdSpecs.size.w,
-        //     this.birdSpecs.size.h
-        // )
+        this.ctx.drawImage(
+            this.imageInstance,
+            this.birdSpecs.pos.x,
+            this.birdSpecs.pos.y,
+            this.birdSpecs.size.w,
+            this.birdSpecs.size.h
+        )
 
         game.onGame ? this.move() : null // comprueba que el juego esta en marcha para que el pajaro tenga gravedad
 
@@ -40,40 +40,12 @@ class Bird {
 
 
     jump() {
-        this.jumping = true
         this.birdSpecs.vel.y = 5
-        this.jumpHeigth = this.birdSpecs.pos.y + 50
     }
-
-    // jump() {
-    //     this.posY -= 40;
-    //     this.velY -= 8;
-    // }
-
 
     move() {
-        if (this.jumping) {
-
-            this.birdSpecs.pos.y -= this.birdSpecs.vel.y
-            this.birdSpecs.vel.y -= this.gravity
-
-            // if (this.birdSpecs.pos.y <= this.jumpHeigth) this.jumping=false
-
-        } else {
-            this.birdSpecs.pos.y -= this.birdSpecs.vel.y
-            this.birdSpecs.vel.y = this.gravity
-        }
+        this.birdSpecs.pos.y -= this.birdSpecs.vel.y
+        this.birdSpecs.vel.y -= this.gravity
     }
-
-    // move() {
-    //     if (this.posY < this.posY0) {   // Está saltando!
-    //     this.posY += this.velY;
-    //     this.velY += this.gravity;
-    //     } else {
-    //     this.posY = this.posY0;
-    //     this.velY = 1;
-    //     }
-    // }
-
     
 }
